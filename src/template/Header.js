966eeb682d3f1ac2bfafeac320f928b3cefce071
Jesús Calderas <calderasjesus0@gmail.com>
2021-01-22 18:53:07 -0500

    reparando el home

--- src/template/Header.js
+++ src/template/Header.js
@@ -2,7 +2,7 @@ const Header = () => {
   const view = `
     <div class="header-main">
       <div class="header-logo">
-        <h1><a href="/">100tii</a></h1>
+        <h1><a href="/spa-rickandmorty">100tii</a></h1>
       </div>
       <div class="header-nav">
         <a href="#/about">
